6bcd529581194a22758e31907012fcdd1bbf7d34
Steffen Bleul <blacklabelops@itbleul.de>
2015-07-18 19:01:45 +0200

    Fixed port collision when setting a slave port.

--- groovy/setSlavesPort.groovy
+++ groovy/setSlavesPort.groovy
@@ -1,9 +1,14 @@
 import jenkins.model.*
+import java.util.logging.Logger
 
+def logger = Logger.getLogger("")
 def instance = Jenkins.getInstance()
-if (instance.getSlaveAgentPort()==-1) {
-instance.setSlaveAgentPort(50000)
-println "Slaveport set!"
+def current_slaveport = instance.getSlaveAgentPort()
+def defined_slaveport = 50000
+
+if (current_slaveport!=defined_slaveport) {
+instance.setSlaveAgentPort(defined_slaveport)
+logger.info("Slaveport set to " + defined_slaveport)
 }
 
 
--- imagescripts/docker-entrypoint.sh
+++ imagescripts/docker-entrypoint.sh
@@ -40,11 +40,19 @@ fi
 if [ -n "${JENKINS_SLAVEPORT}" ]; then
   cat > ${JENKINS_HOME}/init.groovy.d/setSlaveport.groovy <<_EOF_
 import jenkins.model.*
-import hudson.security.*
+import java.util.logging.Logger
 
+def logger = Logger.getLogger("")
 def instance = Jenkins.getInstance()
-instance.setSlaveAgentPort(${JENKINS_SLAVEPORT})
+def current_slaveport = instance.getSlaveAgentPort()
+def defined_slaveport = ${JENKINS_SLAVEPORT}
+
+if (current_slaveport!=defined_slaveport) {
+instance.setSlaveAgentPort(defined_slaveport)
+logger.info("Slaveport set to " + defined_slaveport)
 instance.save()
+instance.doSafeRestart()
+}
 _EOF_
   cat ${JENKINS_HOME}/init.groovy.d/setSlaveport.groovy
 fi
