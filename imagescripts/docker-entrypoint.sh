792267921adb69183e6192fda48c7774abf25fbf
Steffen Bleul <blacklabelops@itbleul.de>
2015-08-08 19:54:40 +0200

    Limiting the log file size to 1GB.

--- imagescripts/docker-entrypoint.sh
+++ imagescripts/docker-entrypoint.sh
@@ -191,7 +191,8 @@ fi
 chown -R jenkins:jenkins ${JENKINS_HOME}
 
 if [ "$1" = 'jenkins' ]; then
-  jenkins_command='java -Dfile.encoding=UTF-8 '${java_vm_parameters}' -jar /opt/jenkins/jenkins.war '${jenkins_parameters}' 2>&1 | tee -a /var/log/jenkins.log'
+  size=$((1024*1024*1024))
+  jenkins_command='java -Dfile.encoding=UTF-8 '${java_vm_parameters}' -jar /opt/jenkins/jenkins.war '${jenkins_parameters}' 2>&1 | head -c '${size}' >> /var/log/jenkins.log'
   runuser -l jenkins -c "${jenkins_command}"
 fi
 
