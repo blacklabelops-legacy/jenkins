2e65d0accc8c7a36087ae2b67ad4ef5331406f5d
Steffen Bleul <blacklabelops@itbleul.de>
2015-11-10 21:07:18 +0100

    AAdded zero day exploit fix for jenkins by adding jenkins-ci groovy script for shutting down cli.

--- imagescripts/docker-entrypoint.sh
+++ imagescripts/docker-entrypoint.sh
@@ -15,6 +15,26 @@ jenkins_plugins=""
 
 zip -d /jenkins/war/WEB-INF/lib/commons-collections-3.2.1.jar org/apache/commons/collections/functors/InvokerTransformer.class || :
 
+cat > ${JENKINS_HOME}/init.groovy.d/cli-shutdown.groovy <<_EOF_
+import jenkins.*;
+import jenkins.model.*;
+import hudson.model.*;
+
+// disabled CLI access over TCP listener (separate port)
+def p = AgentProtocol.all()
+p.each { x ->
+  if (x.name.contains("CLI")) p.remove(x)
+}
+
+// disable CLI access over /cli URL
+def removal = { lst ->
+  lst.each { x -> if (x.getClass().name.contains("CLIAction")) lst.remove(x) }
+}
+def j = Jenkins.instance;
+removal(j.getExtensionList(RootAction.class))
+removal(j.actions)
+_EOF_
+
 if [ -n "${JAVA_VM_PARAMETERS}" ]; then
   java_vm_parameters=${JAVA_VM_PARAMETERS}
 fi
