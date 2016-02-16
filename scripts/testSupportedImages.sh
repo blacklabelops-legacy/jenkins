27aaf8f42e871f91e58861cc68da9b445e442e8a
Steffen Bleul <sbl@itbleul.de>
2016-02-16 15:08:32 +0100

    New test script.

---
+++ scripts/testSupportedImages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+set -o errexit    # abort script at first error
+
+# Setting environment variables
+readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+printf '%b\n' ":: Reading release config...."
+source $CUR_DIR/release.cfg
+
+readonly TEST_JENKINS_VERSION=$JENKINS_VERSION
+readonly TEST_JENKINS_STABLE_VERSION=$JENKINS_STABLE_VERSION
+
+function testImage() {
+  local tagname=$1
+  local port=$2
+  docker run -d -p $port:8080 --name=$tagname blacklabelops/jenkins:$tagname
+  sleep 10
+  curl --retry 10 --retry-delay 5 -v http://localhost:$port
+}
+
+testImage latest 8090
+testImage $TEST_JENKINS_VERSION 8100
+testImage $TEST_JENKINS_STABLE_VERSION 8110
+testImage alpine 8120
+testImage alpine.$TEST_JENKINS_VERSION 8130
+testImage alpine.$TEST_JENKINS_STABLE_VERSION 8140
+testImage war-rc 8150
+testImage war-stable-rc 8160
