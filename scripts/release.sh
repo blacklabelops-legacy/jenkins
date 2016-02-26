3810228dcf16363753cb78757d47f42ea4ba4055
Steffen Bleul <sbl@itbleul.de>
2016-02-26 09:30:02 +0100

    New release version

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,5 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JENKINS_VERSION=1.649
-export JENKINS_STABLE_VERSION=1.642.1
+export JENKINS_VERSION=1.650
+export JENKINS_STABLE_VERSION=1.642.2
