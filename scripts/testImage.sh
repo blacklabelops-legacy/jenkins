f1e926b6018369bab43d54c0ba609e318fe6efd7
Steffen Bleul <blacklabelops@itbleul.de>
2016-04-04 23:20:17 +0200

    Improved test routine with 5 minutes retry.

--- scripts/testImage.sh
+++ scripts/testImage.sh
@@ -5,9 +5,20 @@ set -o errexit    # abort script at first error
 function testImage() {
   local tagname=$1
   local port=$2
+  local iteration=0
   docker run -d -p $port:8080 --name=$tagname blacklabelops/jenkins:$tagname
-  sleep 10
-  curl --retry 10 --retry-delay 10 -v http://localhost:$port
+  while ! curl -v http://localhost:$port
+  do
+      { echo "Exit status of curl: $?"
+        echo "Retrying ..."
+      } 1>&2
+      if [ "$iteration" = '30' ]; then
+        break
+      else
+        ((iteration=iteration+1))
+      fi
+      sleep 10
+  done
   docker stop $tagname
 }
 
