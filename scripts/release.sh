37b921138788d6294165f49ab6a2ded3c1096996
Steffen Bleul <blacklabelops@itbleul.de>
2016-05-14 10:08:59 +0200

    New Jenkins Verion

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,5 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JENKINS_VERSION=2.2
-export JENKINS_STABLE_VERSION=1.651.1
+export JENKINS_VERSION=2.3
+export JENKINS_STABLE_VERSION=1.651.2
