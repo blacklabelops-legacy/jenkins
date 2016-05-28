522704f2d41ad47543bf552f319dc1e22085dd81
Steffen Bleul <blacklabelops@itbleul.de>
2016-05-28 21:41:24 +0200

    Correcting cli adding option.

--- buildscripts/buildImage.sh
+++ buildscripts/buildImage.sh
@@ -8,7 +8,7 @@ function buildImage() {
   local tagname=$3
   local dockerfile=$4
   local cliversion=$5
-  if [[ -z "${param// }" ]]; then
+  if [[ -z "${cliversion// }" ]]; then
     docker build --no-cache -t blacklabelops/jenkins:$tagname --build-arg JENKINS_RELEASE=$release --build-arg JENKINS_VERSION=$version -f $dockerfile .
   else
     docker build --no-cache -t blacklabelops/jenkins:$tagname --build-arg JENKINS_RELEASE=$release --build-arg JENKINS_VERSION=$version --build-arg JENKINS_CLI_VERSION=$cliversion -f $dockerfile .
