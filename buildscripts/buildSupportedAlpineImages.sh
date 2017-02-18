64a800d3e83e321158f7950a48d6f5bacd2a29e4
Steffen Bleul <sbl@itbleul.de>
2017-02-18 20:37:31 +0100

    Correction Dockerfile build reference.

--- buildscripts/buildSupportedAlpineImages.sh
+++ buildscripts/buildSupportedAlpineImages.sh
@@ -11,8 +11,8 @@ source $CUR_DIR/release.sh
 readonly BUILD_JENKINS_VERSION=$JENKINS_VERSION
 readonly BUILD_JENKINS_STABLE_VERSION=$JENKINS_STABLE_VERSION
 
-source $CUR_DIR/buildImage.sh war $BUILD_JENKINS_VERSION latest DockerfileAlpine $BUILD_JENKINS_VERSION
-source $CUR_DIR/buildImage.sh war $BUILD_JENKINS_VERSION $BUILD_JENKINS_VERSION DockerfileAlpine $BUILD_JENKINS_VERSION
-source $CUR_DIR/buildImage.sh war-stable $BUILD_JENKINS_STABLE_VERSION $BUILD_JENKINS_STABLE_VERSION DockerfileAlpine $BUILD_JENKINS_STABLE_VERSION
-source $CUR_DIR/buildImage.sh war-rc latest rc DockerfileAlpine
-source $CUR_DIR/buildImage.sh war-stable-rc latest stable-rc DockerfileAlpine
+source $CUR_DIR/buildImage.sh war $BUILD_JENKINS_VERSION latest Dockerfile $BUILD_JENKINS_VERSION
+source $CUR_DIR/buildImage.sh war $BUILD_JENKINS_VERSION $BUILD_JENKINS_VERSION Dockerfile $BUILD_JENKINS_VERSION
+source $CUR_DIR/buildImage.sh war-stable $BUILD_JENKINS_STABLE_VERSION $BUILD_JENKINS_STABLE_VERSION Dockerfile $BUILD_JENKINS_STABLE_VERSION
+source $CUR_DIR/buildImage.sh war-rc latest rc Dockerfile
+source $CUR_DIR/buildImage.sh war-stable-rc latest stable-rc Dockerfile
