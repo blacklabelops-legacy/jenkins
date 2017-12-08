cda7053ba8c5da079cb992f18e7881a2198d2725
Steffen Bleul <sbl@itbleul.de>
2017-12-08 13:47:01 +0100

    New jenkins versions.

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,5 +3,5 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export JENKINS_VERSION=2.92
-export JENKINS_STABLE_VERSION=2.73.3
+export JENKINS_VERSION=2.93
+export JENKINS_STABLE_VERSION=2.89.1
